3c8f242eaa6c5c4404e4ff5f597d2fa9a6c72a1e
LitStark <ysangeet7@gmail.com>
2026-01-06 04:21:51 +0530

    Add Fallback (Create DB IFNOTEXIST & Secure Credentials into .env)

--- db.py
+++ db.py
@@ -1,9 +1,22 @@
 from sqlalchemy import create_engine, Column, Integer, String, Enum, DATETIME, DECIMAL, ForeignKey,TIMESTAMP, DATE
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.sql import func
+import os
+# without this show NONE values in os.getenv()
+from dotenv import load_dotenv  
+
+# Load environment variables from .env file for python readability (for linux and docker not needed)
+load_dotenv()       
+
+# Load Database configuration from environment variables
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 engine = create_engine(
-    "mysql+pymysql://root:Mysql%401234@localhost:3306/stockprofitmonitor",echo= False,future= True,
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",echo= False,future= True,
     pool_pre_ping=True
 )
 
@@ -30,3 +43,8 @@ class wallet_transactions(base):
     amount = Column(DECIMAL(12, 2), nullable=False)
     ref_trade_id = Column(Integer, ForeignKey("trade_transactions.trade_id"), nullable=True)
     created_at = Column(DATETIME, server_default=func.now(), nullable=False)
+
+# Create tables if they don't exist
+def init_db():
+    base.metadata.create_all(bind=engine)       
+
